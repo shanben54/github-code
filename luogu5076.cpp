47dd4ecb25e1479dc155789cf8c263cc03db1d0f
shanben54 <3043996313@qq.com>
2026-06-08 22:33:01 +0800

    luogu5076.1.cpp

---
+++ luogu5076.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define maxn 100010
+int n,root,cnt,opt,x;
+struct node{
+    int left,right,size,value,num;
+    node(int left1,int right1,int size1,int value1):left(left1),right(right1),size(size1),value(value1),num(1){}
+    node(){}
+}t[maxn];
+inline void update(int root){
+    t[root].size=t[t[root].left].size + t[t[root].right].size + t[root].num;
+}
+int rak(int x,int root){
+    if(root){
+        if(x<t[root].value){
+            return rak(x,t[root].left);
+        }
+    }
+}
