98019bdcf1074aa7fbb85024bb8522badf0db1fc
shanben54 <3043996313@qq.com>
2026-06-14 21:49:32 +0800

    luogu1536.cpp

---
+++ luogu1536.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fa[1010];
+int n,m,x,y;
+int find(int a){
+    if(fa[a]==a) return a;
+    return fa[a]=find(fa[a]);
+}
+void join(int a,int b){
+    int a1=find(a),b1=find(b);
+    if(a1!=b1){
+        fa[a1]=b1;
+    }
+}
+int main(){
+    while(cin>>n&&n!=0){
+        int ans=0;
+        cin>>m;
+        for(int i=1;i<=n;i++){
+            fa[i]=i;
+        }
+        for(int i=0;i<m;i++){
+            cin>>x>>y;
+            join(x,y);
+        }
+        for(int i=1;i<=n;i++){
+            if(find(i)==i) ans++;
+        }
+        cout<<ans-1<<endl;
+    }
+    return 0;
+}
