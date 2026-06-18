6690d64c49c822443a7858091cc4347d6c762875
shanben54 <3043996313@qq.com>
2026-06-18 10:36:10 +0800

    luogu3370.2.cpp

--- luogu3370.2.cpp
+++ luogu3370.2.cpp
@@ -1,3 +1,5 @@
+//完整版的luogu3370，给定一定的字符串，统计有多少个不同的字符串
+//有了简化版的思路，就是取模储存，只是计算字符串的hash值(对mod取模的值)不太一样
 #include<bits/stdc++.h>
 using namespace std;
 #define maxn 1510
@@ -9,11 +11,11 @@ vector<string> linker[mod+2];
 inline void insert(){
     int hash=1;
     for(int i=0;s[i];i++){
-        hash=(hash*1ll*base+s[i])%mod;
+        hash=(hash*1ll*base+s[i])%mod;//计算这个字符串的hash值
     }
     string t=s;
     for(int i=0;i<linker[hash].size();i++){
-        if(linker[hash][i]==t) return;
+        if(linker[hash][i]==t) return;//对取模为hash的数进行枚举，看看有没有重复出现
     }
     linker[hash].push_back(t);
     ans++;
