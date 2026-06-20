d87cd6e2ddc3488b1ce3667f7f79363da6237170
shanben54 <3043996313@qq.com>
2026-06-20 23:13:06 +0800

    luogu5250.cpp

--- luogu5250.cpp
+++ luogu5250.cpp
@@ -1,3 +1,4 @@
+//木材仓库问题，一个仓库储存一定量的木头，没有长度相同的木材，现可以进行存放，取出长度为lenth的木材，如果没有就取最接近的，有多根取最短的
 #include<bits/stdc++.h>
 using namespace std;
 int n,opt,lenth;
@@ -7,17 +8,18 @@ int main(){
     while(n--){
         cin>>opt>>lenth;
         if(opt==1){
-            if(ds.find(lenth)!=ds.end()) cout<<"Already Exist"<<endl;
+            if(ds.find(lenth)!=ds.end()) cout<<"Already Exist"<<endl;//不能有两根一样长的木材
+            //如果不存在的话，find会=回返回end()
             else ds.insert(lenth);
         }
         else if(ds.empty()){
             cout<<"Empty"<<endl;
         }
         else{
-            set<int>::iterator i=ds.lower_bound(lenth),j=i;
-            if(j!=ds.begin()) --j;
-            if(i!=ds.end()&&lenth-(*j)>(*i)-lenth) j=i;
-            cout<<(*j)<<endl,ds.erase(j);
+            set<int>::iterator i=ds.lower_bound(lenth),j=i;//lower_bound返回不小于lenth的最小的数的地址
+            if(j!=ds.begin()) --j;//如果j是begin()的话就不可以自减
+            if(i!=ds.end()&&lenth-(*j)>(*i)-lenth) j=i;//i如果是end()查询不到其的值，如果i和j离lenth的距离一样取较小的j，所以是大于号
+            cout<<(*j)<<endl,ds.erase(j);//取出后删除
         }
     }
     return 0;
--- luogu5266.cpp
+++ luogu5266.cpp
@@ -1,3 +1,5 @@
+//学籍管理系统，可以储存每个学生的名字和分数，我感觉像python里面的字典
+//使用了stl里面的map，可以储存两种类型的数据，第一个为下标，第二个为储存的数据
 #include<bits/stdc++.h>
 using namespace std;
 int n,opt,num,ans;
@@ -7,19 +9,19 @@ int main(){
     cin>>n;
     while(n--){
         cin>>opt;
-        if(opt==1){
+        if(opt==1){//插入和修改
             cin>>name>>num;
-            ds[name]=num;
+            ds[name]=num;//不需要插入数据，如果你访问的ds[name]不存在，则会自动创建一个ds[name]，里面的数据为默认值
             cout<<"OK"<<endl;
-        }else if(opt==2){
+        }else if(opt==2){//查询
             cin>>name;
-            if(ds.find(name)!=ds.end()){
+            if(ds.find(name)!=ds.end()){//和set一样，如果数据不存在，find会返回end()
                 cout<<ds[name]<<endl;
             }
             else{
                 cout<<"Not found"<<endl;
             }
-        }else if(opt==3){
+        }else if(opt==3){//删除
             cin>>name;
             if(ds.find(name)!=ds.end()){
                 ds.erase(ds.find(name));
@@ -27,7 +29,7 @@ int main(){
             }else{
                 cout<<"Not found"<<endl;
             }
-        }else{
+        }else{//汇总
             cout<<ds.size()<<endl;
         }
     }
