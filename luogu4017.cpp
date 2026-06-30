19e79394f0ec16a69cdf26b5bfc79ed2cbba4673
shanben54 <3043996313@qq.com>
2026-06-30 23:06:33 +0800

    luogu4017.1.cpp

--- luogu4017.cpp
+++ luogu4017.cpp
@@ -1,9 +1,12 @@
-//今天居然忘记写代码了，罪过罪过，明天一定补上
+//食物链问题，给定一些捕食关系，求有多少条完整的食物链
+//这也是个DAG，并且运用了拓扑排序
+//f[x]表示从0入度点到x的食物链数，先将入度为0的结点入栈，然后遍历其链接的点，然后f[y]+=f[x]，并使y的入度--；
+//如果y的入度也为0了就将其入栈，直到栈空为止
 #include<bits/stdc++.h>
 using namespace std;
 #define maxn 5005
 #define maxm 500005
-#define mod 80112002
+#define mod 80112002//取模数
 int n,m,ans;
 vector<int>p[maxn];
 queue<int>q;
@@ -12,31 +15,31 @@ int main(){
     cin>>n>>m;
     for(int i=1;i<=m;i++){
         int x,y;
-        cin>>x>>y;
+        cin>>x>>y;//输入捕食关系
         outd[x]++;
         ind[y]++;
-        p[x].push_back(y);
+        p[x].push_back(y);//邻接表，记录这个捕食关系，x指向y
     }
     memset(f,0,sizeof(f));
     for(int i=1;i<=n;i++){
         if(ind[i]==0){
-            q.push(i);
-            f[i]=1;
+            q.push(i);//先把所有入度为0的结点入栈，作为起点
+            f[i]=1;//使入度为0的结点的f值为1
         }
     }
-    while(!q.empty()){
+    while(!q.empty()){//栈空为止，说明所有结点都更新完毕
         int x=q.front();
         q.pop();
         for(int i=0,sz=p[x].size();i<sz;i++){
             int y=p[x][i];
-            f[y]=(f[x]+f[y])%mod;
-            ind[y]--;
-            if(ind[y]==0) q.push(y);
+            f[y]=(f[x]+f[y])%mod;//需要取模，防止数据过大
+            ind[y]--;//计算完y的一个入度结点，y的入度-1
+            if(ind[y]==0) q.push(y);//入度为0时加入栈
         }
     }
     for(int i=1;i<=n;i++){
-        if(outd[i]==0){
-            ans=(ans+f[i])%mod;
+        if(outd[i]==0){//统计所有出度为0的点，也就是食物链的最底层
+            ans=(ans+f[i])%mod;//总链数结果也要取模，防止数据过大
         }
     }
     cout<<ans<<endl;
