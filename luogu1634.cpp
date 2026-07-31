18c12e0986b60c329ae4cd9ffd67e1eaff72e329
shanben54 <3043996313@qq.com>
2026-07-31 23:47:15 +0800

    阿巴阿巴

--- luogu1634.cpp
+++ luogu1634.cpp
@@ -1,4 +1,4 @@
-//第一次提交还没通过，思索半天发现把int改成long lon就可以了，这提醒我要注意一下数据范围和类型#include<bits/stdc++.h>
+//第一次提交还没通过，思索半天发现把int改成long lon就可以了，这提醒我要注意一下数据范围和类型
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
