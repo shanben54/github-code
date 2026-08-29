5f514b6601dd7f25b5d7f42b5ffa6dd8e5e05cb7
shanben54 <3043996313@qq.com>
2026-08-29 10:33:01 +0800

    关键路径

--- CriticalPath.c
+++ CriticalPath.c
@@ -1,3 +1,4 @@
+//求AOE图的关键路径
 #include<stdio.h>
 
 typedef char VertexType;
@@ -26,76 +27,81 @@ typedef struct{
 }graphAdjList,*GraphAdjList;
 
 
-int *etv,*ltv;
-int *stack2;
-int top2;
+int *etv,*ltv;//事件(顶点)最早发生时间和最迟发生时间
+int *stack2;//储存拓扑排序的栈
+int top2;//stcak2的指针下标
 
-//
+//拓扑排序
+//在拓扑排序的过程中，找到所以顶点的最早发生时间
 Status TopologicalSort(GraphAdjList GL){
     EdgeNode *e;
     int i,k,gettop;
-    int top=0;
-    int count=0;
-    int *stack;
-    stack=(int *)malloc(GL->numVertexes * sizeof(int));
+    int top=0;//栈指针下标
+    int count=0;//统计顶点数
+    int *stack;//入度为0的顶点栈
+    stack=(int *)malloc(GL->numVertexes * sizeof(int));//初始化开辟内存
     for(i=0;i<GL->numVertexes;i++){
         if(GL->adjList[i].in==0){
-            stack[++top]=i;
+            stack[++top]=i;//找到入度为0的顶点入栈
         }
     }
     top2=0;
-    etv=(int *)malloc(GL->numVertexes * sizeof(int));
+    etv=(int *)malloc(GL->numVertexes * sizeof(int));//开辟内存
     for(i=0;i<GL->numVertexes;i++){
-        etv[i]=0;
+        etv[i]=0;//初始化为0
     }
     stack2=(int *)maoolc(GL->numVertexes * sizeof(int));
     while(top!=0){
-        gettop=stack[top--];
-        count++;
-        stack2[++top2]=gettop;
-        for(e=GL->adjList[gettop].firstedge;e;e=e->next){
+        gettop=stack[top--];//取出栈首顶点
+        count++;//计数加1
+        stack2[++top2]=gettop;//将取出的顶点加入拓扑排序的栈
+        for(e=GL->adjList[gettop].firstedge;e;e=e->next){//遍历当前顶点的邻接点
             k=e->adjvex;
             if(!(--GL->adjList[k].in)){
-                stack[++top]=k;
+                stack[++top]=k;//入度为0的顶点加入栈里
             }
             if((etv[gettop]+e->weight)>etv[k]){
-                etv[k]=etv[gettop]+e->weight;
+                etv[k]=etv[gettop]+e->weight;//某个事件(顶点)的最早发生时间是所有前一个顶点加权值的最大值，找到这个最大值
             }
         }
     }
-    if(count<GL->numVertexes){
+    if(count<GL->numVertexes){//排序的顶点数少于总顶点数，说明存在环
         return error;
     }else{
         return ok;
     }
 }
 
-//
+//求关键路径
+/*先求出顶点的最早发生时间和最晚发生时间，边的最早最晚发生时间可以用边的来求得，如果一个边的最早发生时间和最晚发生时间相同，
+那这个边就是关键活动，从头遍历所有边，找到所有关键活动，就求得了关键路径*/
 void CriticalPath(GraphAdjList GL){
     EdgeNode *e;
     int i,gettop,j,k;
-    int ete,lte;
-    TopologicalSort(GL);
-    ltv=(int *)malloc(GL->numVertexes * sizeof(int));
+    int ete,lte;//活动(边)最早发生时间和最晚发生时间
+    TopologicalSort(GL);//对图进行排序，求出stack2和etv
+    ltv=(int *)malloc(GL->numVertexes * sizeof(int));//对ltv开辟内存
     for(i=0;i<GL->numVertexes;i++){
-        ltv[i]=etv[GL->numVertexes-1];
+        ltv[i]=etv[GL->numVertexes-1];//对ltv进行初始化
     }
     while(top2!=0){
-        gettop=stack2[top2--];
-        for(e=GL->adjList[gettop].firstedge;e;e=e->next){
+        gettop=stack2[top2--];//取出栈首顶点，也就是拓扑排序倒着取出顶点
+        for(e=GL->adjList[gettop].firstedge;e;e=e->next){//遍历邻接点
             k=e->adjvex;
             if(ltv[k]-e->weight<ltv[gettop]){
-                ltv[gettop]=ltv[k]+e->weight;
+                ltv[gettop]=ltv[k]+e->weight;//事件最晚发生时间就是所有邻接点减去权值的最小值，找到最小值
             }
         }
     }
+
+    //求ete,lte和关键活动
     for(j=0;j<GL->numVertexes;j++){
-        for(e=GL->adjList[j].firstedge;e;e=e->next){
+        for(e=GL->adjList[j].firstedge;e;e=e->next){//从头遍历所有的边，j指向k
             k=e->adjvex;
-            ete=etv[j];
-            lte=ltv[k]-e->weight;
-            if(ete==lte){
-                printf("<v%d - v%d> length: %d\n",GL->adjList[j].data,GL->adjList[k].data,e->weight);
+            ete=etv[j];//活动最早发生时间就是头顶点的最早发生时间
+            lte=ltv[k]-e->weight;//活动最晚发生时间就是尾顶点最晚发生时间减去权值
+            if(ete==lte){//如果最早发生时间等于最晚发生时间，说明这个活动是关键活动
+                printf("<v%d - v%d> length: %d\n",GL->adjList[j].data,GL->adjList[k].data,e->weight);//打印数据
             }
         }
     }
