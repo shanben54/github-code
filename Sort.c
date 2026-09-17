13d96ebc7b809b1c981bbba86f76f3755cb2efc1
shanben54 <3043996313@qq.com>
2026-09-17 23:10:16 +0800

    装棣卿-9.17

--- Sort.c
+++ Sort.c
@@ -229,3 +229,33 @@ void MergePass(int SR[],int TR[],int s,int n){//将SR中相邻长度为s的子�
         }
     }
 }
+
+//快速排序
+void QuickSort(SqList *L){
+    QSort(L,1,L->length);
+}
+
+void QSort(SqList *L,int low,int high){
+    int pivot;
+    if(low<high){
+        pivot=Partition(L,low,high);
+        QSort(L,low,pivot-1);
+        QSort(L,pivot+1,high);
+    }
+}
+
+int Partition(SqList *L,int low,int high){
+    int pivotkey;
+    pivotkey=L->r[low];
+    while(low<high){
+        while(low<high&&L->r[high]>=pivotkey){
+            high--;
+        }
+        swap(L,low,high);
+        while(low<high&&L->r[low]<=pivotkey){
+            low++;
+        }
+        swap(L,low,high);
+    }
+    return low;
+}
